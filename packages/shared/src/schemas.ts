27b5b3145e97560042c7ac2d71e5d739e9ee4757
vishal123k <vishalk38916@gmail.com>
2026-04-27 13:03:05 +0530

    changes

--- packages/shared/src/schemas.ts
+++ packages/shared/src/schemas.ts
@@ -2,7 +2,6 @@ import { z } from 'zod';
 
 export const joinWaitlistSchema = z.object({
   email: z.string()
-
     .email({ message: "Please enter a valid email address format" })
     .endsWith("@gmail.com", { message: "invalid email" }),
   referred_by: z.string().optional()
